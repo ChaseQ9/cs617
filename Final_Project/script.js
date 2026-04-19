423af3a60b3367491a2b6fe7babcd7394c1bde2a
Chase Quigley <chasequigley9@gmail.com>
2026-04-19 12:29:58 -0400

    basic update

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,13 +1,98 @@
 
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 200;
 
 const dataVisOptions = {
 
 }
 window.onload = () => {
 	createZipVsAvgTotalVal();
+	createPropertyPie("#vis2");
+	createPropertyScatter("#vis3");
 }
 
+function createPropertyScatter(vis) {
+	let spec = {
+		"title": "Scatter Plot Land SQFT vs Gross Tax",
+		// Load in the data, set the format
+		"data": {
+			"url": "./prop.json",
+			"format": {
+				"type": "json",
+			}
+		},
+		"width": CHART_WIDTH,
+		"height": CHART_HEIGHT,
+		// Set the 'mark' of the chart, aka what type of chart we are using 
+		"mark": "point",
+		// Convert the zip code column to a string (prevents losing the 0)
+		"background": "white",
+		// Set the encoding of the graph
+		"encoding": {
+			"x": {
+				"aggregate": "average",
+				"field": "LAND_SF",
+				"type": "quantitative",
+				"title": "Land Square Footage"
+			},
+			"y": {
+				"field": "GROSS_TAX",
+				"type": "quantitative",
+				"title": "Gross Tax"
+			}
+		},
+	};
+	vegaEmbed(vis, spec, {"actions": false});
+}
 
+function createPropertyPie(vis) {
+	let spec = {
+		"title": "Property Breakdown By Land Usage(LU)",
+		// Load in the data, set the format
+		"data": {
+			"url": "./prop.json",
+			"format": {
+				"type": "json",
+			}
+		},
+		"width": CHART_WIDTH,
+		"height": CHART_HEIGHT,
+		"transform": [ {
+			"aggregate": [{ "op" : "count", "as": "count"}],
+			"groupby": ["LU"]
+		},
+		{
+			"joinaggregate": [
+				{"op": "sum", "field": "count", "as" : "total"}]
+		},
+		{
+			"calculate": "datum.count / datum.total",
+			"as" : "percent"
+		}],
+		// Set the 'mark' of the chart, aka what type of chart we are using 
+		"mark": {
+			"type": "arc",
+			"innerRadius": 50
+		},
+		// Convert the zip code column to a string (prevents losing the 0)
+		"background": "white",
+		// Set the encoding of the graph
+		"encoding": {
+			"theta": {
+				"field": "percent",
+				"type": "quantitative"
+			},
+			"color": {
+				"field": "LU",
+				"type": "nominal",
+			},
+			"tooltip": [
+				{"field": "percent", "type": "quantitative", "format": ".1%"}
+			],
+		}
+	};
+	vegaEmbed(vis, spec, {"actions": false});
+}
 function createZipVsAvgTotalVal() {
 	// This function used chatgpt for assistance in creating and fixing the
 	// vegalite code seen below
@@ -20,7 +105,8 @@ function createZipVsAvgTotalVal() {
 				"type": "json",
 			}
 		},
-		"width": "1000",
+		"width": CHART_WIDTH,
+		"height": CHART_HEIGHT,
 		// Set the 'mark' of the chart, aka what type of chart we are using 
 		"mark": {
 			"type": "bar",
@@ -51,10 +137,9 @@ function createZipVsAvgTotalVal() {
 			"tooltip": [
 				{"field": "ZIP_STR", "type": "nominal", "title": "Zip"},
 				{"field": "CITY", "type": "nominal"},
-				{"aggregate" : "mean", "field": "LIVING_AREA", "title": "Avg Living Area"},
-				{"aggregate" : "mean", "field": "TOTAL_VALUE", "title": "Avg Value"},
+				{"aggregate" : "mean", "field": "TOTAL_VALUE", "title": "Avg Value $"},
 			]
 		}
-	}
+	};
 	vegaEmbed('#vis', spec, {"actions": false});
 }
