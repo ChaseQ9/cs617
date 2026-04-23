a6b0548185d3e0cb57934bdc4b9352cdd65fd52e
Charles J Quigley <Chasequigley9@gmail.com>
2026-04-22 23:06:08 -0400

    basic update

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,52 +1,37 @@
+// Chase Quigley
+// CS617 
+let width = 600;
+let height = 600;
+let marginTop,marginBottom,marginRight,marginLeft = 20;
 
-const spec = {
-	"data": {
-		"url": "./data/BruinsData/dataJSON/bos_standardstats.json",
-		"format": "json"
-	},
-	"mark": "bar",
-	"encoding": {
-		"x": {
-			"field": "Age",
-			"type": "quantitative"
-		},
-		"y": {
-			"field": "G",
-			"type": "quantitative"
-		}
-	}
-};
+const formatTime = d3.timeParse("%Y-%m-%d");
+const formatMonth = d3.timeFormat("%B");
+// Dow = Day of week
+const formatDow = d3.timeFormat("%A");
 
-let groupBar = {
-	"data": {
-		"url": "./data/BruinsData/dataJSON/bos_seasonresults.json",
-		"format": "json"
-	},
-	"mark": "line",
-	"encoding": {
-		"x": {
-			"field": "Date",
-			"type": "nominal"
-		},
-		"y": {
-			"field": "GF",
-			"type": "quantitative"
-		},
-	}
-};
+const x_scale = d3.scaleBand().range([0, width]);
+const y_scale = d3.scaleLinear().range([height, 0]);
 
-function vis(spec) {
-	vegaEmbed("#vis", spec, {actions: false});
+const svg = d3.select("vis").attr("width", width).attr("height", height);
+
+function createResultsChart() {
+	d3.csv("./data/BruinsData/seasonresults.csv").then(function(data) {
+		// Everything mainly happens between this 
+		console.log(data);
+		data.forEach((d) => {
+			let time = formatTime(d.Date);
+			console.log(formatMonth(time));
+			console.log(formatDow(time));
+			console.log(d.Result);
+			//console.log(d["Att."]);
+		});
+		// Everything above is related to drawing the graphs
+	}).catch(function(error) {
+		console.error(error);
+	});
 }
 
-function main(Team) {
-	vis(groupBar);
-}
-
-window.onload = () => {
-	let Bruins = document.getElementById("Bruins");
-	let RedSox = document.getElementById("RedSox");
-	let Celtics = document.getElementById("Celtics");
-	let Patriots = document.getElementById("Patriots");
-	main(Bruins);
+window.onload = (e) => {
+	createResultsChart();
+	console.log("test");
 }
